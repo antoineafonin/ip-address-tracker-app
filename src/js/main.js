e8c50c1eb31f731fb1224fc0c50ee661c60bc5f4
antoineafonin <antoineafonin@gmail.com>
2025-01-24 18:22:51 -0500

    add info about ip

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,39 @@ import { validateIp } from "./helper";
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
 
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
 function getData() {
-    if (validateIp(ipInput.value)) {
-        // check datas
+    const ip = ipInput.value.trim();
+
+    if (!ip) {
+        alert("IP address cannot be empty!");
+        return;
+    }
+
+    if (validateIp(ip)) {
         fetch(
-            `https://geo.ipify.org/api/v2/country?apiKey=at_JVBFeiYLqDz1vZNzvm31WN4r1bEhH&ipAddress=${ipInput.value}`
+            `https://geo.ipify.org/api/v2/country?apiKey=at_JVBFeiYLqDz1vZNzvm31WN4r1bEhH&ipAddress=${ip}`
         )
-            .then((response) => response.json())
-            .then(console.log); 
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(setInfo)
+            .catch((error) => {
+                console.error("Error fetching IP data:", error);
+                alert("Failed to retrieve data. Please try again.");
+            });
+    } else {
+        alert("Please enter a valid IPv4 or IPv6 address.");
     }
 }
 
@@ -21,4 +43,11 @@ function handleKey(e) {
     if (e.key === "Enter") {
         getData();
     }
+}
+
+function setInfo(mapData) {
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = `${mapData.location.country}, ${mapData.location.region}`;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
