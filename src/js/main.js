da165d0f7e572a960c7b33cafd49d7ac735a471e
antoineafonin <antoineafonin@gmail.com>
2025-01-24 18:00:29 -0500

    get api

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+    // check datas
+    fetch(
+        `https://geo.ipify.org/api/v2/country?apiKey=at_JVBFeiYLqDz1vZNzvm31WN4r1bEhH&ipAddress=${ipInput.value}`
+    )
+        .then((response) => response.json())
+        .then(console.log);
+}
+
+function handleKey(e) {
+    if (e.key === "Enter") {
+        getData();
+    }
+}
